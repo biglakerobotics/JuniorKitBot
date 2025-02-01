38ad2b7cf3b62ceef133f3d9c3a71caaf5d2fef8
BigLakeRobotics <biglakerobotics@gmail.com>
2025-02-01 15:20:32 -0600

    Added RevLib and Added some DriveTrain Code

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,10 +1,21 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
-    private 
-    public DriveTrain() {
 
+    SparkMax leftLeader;
+    SparkMax leftFollower;
+    SparkMax rightLeader;
+    SparkMax rightFollower;
+
+    public DriveTrain() {
+        leftLeader = new SparkMax(1, MotorType.kBrushed);
+        leftFollower = new SparkMax(2, MotorType.kBrushed);
+        rightLeader = new SparkMax(3, MotorType.kBrushed);
+        rightFollower = new SparkMax(4, MotorType.kBrushed);
     }
 }
